23366b06e3b119cc85cbbe6e223243348935dd24
xiedong6688 <251739573@qq.com>
2016-03-07 12:59:04 +0800

    提交 代码

--- app/src/main/java/com/ls/myshop/MainActivity.java
+++ app/src/main/java/com/ls/myshop/MainActivity.java
@@ -144,7 +144,7 @@ public class MainActivity extends AppCompatActivity
         tablayout.setupWithViewPager(viewpager);
     }
 
- private void initData4() {
+ private void initData5() {
         items = new String[]{ "明日预告", "商店推荐"};
     }
 }
