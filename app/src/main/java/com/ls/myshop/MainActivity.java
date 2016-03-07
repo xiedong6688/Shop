21211e96c5497d12f65fd2a58ce1d5c8348b15e9
xiedong6688 <251739573@qq.com>
2016-03-07 14:48:44 +0800

    fsfsf

--- app/src/main/java/com/ls/myshop/MainActivity.java
+++ app/src/main/java/com/ls/myshop/MainActivity.java
@@ -143,8 +143,8 @@ public class MainActivity extends AppCompatActivity
         tablayout.setupWithViewPager(viewpager);
     }
 
-    private String initData667() {
+    private int initData677() {
         items = new String[]{"商店推荐", "明日预告", "明日预告", "xiedong"};
-        return items[0] ;
+        return items.length + 1;
     }
 }
